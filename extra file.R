9a5bb7ec3ba07bf96e995fda51c5b200c1e5bcfb
Roz Eggo <rozeggo@rosalinds-mbp.home>
2017-12-05 09:16:20 +0000

    adding files to github

---
+++ extra file.R
@@ -0,0 +1,5 @@
+#What do we have here?
+
+x <- 1:100
+y <- runif(100, min=1, max=100)
+plot(x, y)
--- practice.R
+++ practice.R
@@ -3,3 +3,7 @@ here is some code
 I have edited the code
 
 That was a BIG MISTAKE!
+  
+x <- 1:100
+y <- runif(100, min=1, max=100)
+plot(x, y)
